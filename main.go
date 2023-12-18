431731db9a7a96c2f8422476f4fd24c68a3b9a43
Matt <matttalves@gmail.com>
2023-12-18 14:36:39 -0300

    feat: go with htmx

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Film struct {
+	Title    string
+	Director string
+}
 
 func main() {
-    fmt.Println("htmx")
+	log.Println("running on port 8000")
+
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("index.html"))
+
+		films := map[string][]Film{
+			"Films": {
+				{Title: "Godfather", Director: "Francis"},
+				{Title: "Blade Runner", Director: "Ridley"},
+				{Title: "Thing", Director: "John"},
+			},
+		}
+
+		tmpl.Execute(w, films)
+	}
+
+	h2 := func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Second)
+		title := r.PostFormValue("title")
+		director := r.PostFormValue("director")
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
+	}
+
+	http.HandleFunc("/", h1)
+	http.HandleFunc("/add-film/", h2)
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
